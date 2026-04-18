c76bcb3c756c6d5d86646ab251536990ec53fca5
Lucien Hsiao <lmhsiao@ucsd.edu>
2026-04-18 15:06:15 -0700

    update navigation menu

--- global.js
+++ global.js
@@ -1,13 +1,43 @@
-console.log('IT’S ALIVE!');
+console.log("IT'S ALIVE!");
 
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$('nav a');
+const BASE_PATH =
+  location.hostname === "localhost" || location.hostname === "127.0.0.1"
+    ? "/"
+    : "/portfolio/";
 
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname
-);
+let pages = [
+  { url: "", title: "Home" },
+  { url: "projects/", title: "Projects" },
+  { url: "contact/", title: "Contact" },
+  { url: "resume/", title: "Resume" },
+  { url: "https://github.com/lucien1010/portfolio", title: "GitHub" }
+];
 
-currentLink?.classList.add('current');
+let nav = document.createElement("nav");
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  if (!url.startsWith("http")) {
+    url = BASE_PATH + url;
+  }
+
+  let a = document.createElement("a");
+  a.href = url;
+  a.textContent = title;
+  nav.append(a);
+
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add("current");
+  }
+
+  if (a.host !== location.host) {
+    a.target = "_blank";
+  }
+}
