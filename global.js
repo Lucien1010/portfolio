5963d06c1f80e619d5d68c346614d82c155d7800
Lucien Hsiao <lmhsiao@ucsd.edu>
2026-04-18 14:53:22 -0700

    global.js update

--- global.js
+++ global.js
@@ -2,7 +2,8 @@ console.log('IT’S ALIVE!');
 
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
-  
+}
+
 let navLinks = $$('nav a');
 
 let currentLink = navLinks.find(
@@ -10,4 +11,3 @@ let currentLink = navLinks.find(
 );
 
 currentLink?.classList.add('current');
-}
