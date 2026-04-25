6875a415ecb2479faee4798196d4056a624d334e
Lucien Hsiao <lmhsiao@ucsd.edu>
2026-04-24 18:34:36 -0700

    counting projects

--- projects/projects.js
+++ projects/projects.js
@@ -4,4 +4,7 @@ const projects = await fetchJSON('../lib/projects.json');
 
 const projectsContainer = document.querySelector('.projects');
 
+const title = document.querySelector('.projects-title');
+title.textContent = `${projects.length} Projects`;
+
 renderProjects(projects, projectsContainer, 'h2');
