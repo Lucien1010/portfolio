fb27377567815345dc0c92df3ff71cc5c91fbeb4
Lucien Hsiao <lmhsiao@ucsd.edu>
2026-04-24 18:12:17 -0700

    Setting up projects.js

--- global.js
+++ global.js
@@ -89,3 +89,18 @@ form?.addEventListener('submit', function (event) {
   console.log(url);
   location.href = url;
 });
+
+export async function fetchJSON(url) {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
